d498ad53fc60173b5fb15fce9131fed66825b367
XeenProof <whosejimmy@gmail.com>
2023-03-27 18:36:23 -0400

    Path Generation Functions Complete, untested

---
+++ src/utils/Pathing/CreatePaths.ts
@@ -0,0 +1,65 @@
+import Vec2 from "../../Wolfie2D/DataTypes/Vec2";
+import RandUtils from "../../Wolfie2D/Utils/RandUtils";
+import PathNode from "./PathNode";
+
+const defaults = {
+    xmin: 100,
+    ymin: 100,
+    xmax: 800,
+    ymax: 450,
+    destNum: 20,
+    mindist: 300,
+    repeating: 0,
+    speed: -1,
+    threshold: 5
+}
+
+export interface Positions{
+    x: number,
+    y: number,
+    repeat?: number,
+    speed?: number,
+    thresh?: number
+}
+
+export function generateRandomPathList(setList: Record<string, any>[]): PathNode[]{
+    let allPathing = setList.map((set) => {return this.generateRandomPath(set)})
+    let path = allPathing.reduce((x, y)=>{return [...x, ...y]}, []);
+    return path;
+}
+
+export function generatePathFromList(list: Array<Positions>, default_speed: number): PathNode[]{
+    return list.map((pos) => {
+        let { x, y, repeat, speed, thresh} = pos;
+        let r = (repeat)?repeat:0;
+        let s = (speed)?speed:default_speed;
+        let t = (thresh)?thresh:defaults.threshold
+        return new PathNode(new Vec2(x,y), r, s, t)
+    })
+}
+
+export function generateRandomPath(set: Record<string, any>): PathNode[]{
+    let settings = {...defaults, ...set}
+    let {destNum, repeating, speed, threshold} = settings
+    let array = new Array<Vec2>(destNum);
+    for(let i = 0; i < destNum; i++){
+        let prev = (i > 0)? array[i-1]:null;
+        array[i] = this.getNextCoord(settings, prev);
+    }
+    let nodes = this.toNodeList(array, repeating, speed, threshold);
+    return nodes;
+}
+
+function toNodeList(list: Array<Vec2>, repeating: number, speed: number, threshold: number){
+    return list.map((vec) => {return new PathNode(vec, repeating, speed, threshold)})
+}
+
+function getNextCoord(settings: Record<string, any>, previous: Vec2 = null){
+    let {xmin, ymin, xmax, ymax, mindist} = settings
+    let nextVec = RandUtils.randVec(xmin, xmax, ymin, ymax)
+    if(previous == null){return nextVec;}
+    while(nextVec.distanceTo(previous) < mindist){
+        nextVec = RandUtils.randVec(xmin, xmax, ymin, ymax, nextVec)
+    }
+    return nextVec;
+}
---
+++ src/utils/Pathing/PathNode.ts
@@ -0,0 +1,25 @@
+import Vec2 from "../../Wolfie2D/DataTypes/Vec2";
+
+
+export default class PathNode {
+    private _position: Vec2;
+    private _repeatCount: number;
+    private _speed: number;
+    private _distanceThreshold: number; /** The distance a node must be to a point to consider it as having arrived */
+    
+    
+    constructor(position: Vec2 = Vec2.ZERO, repeatCount:number = 0, speed:number = -1, distanceThreshold: number = -1){
+        this._position = position;
+        this._repeatCount = repeatCount;
+        this._speed = speed;
+        this._distanceThreshold = distanceThreshold;
+    }
+
+    public get position(): Vec2 {return this._position;}
+    public get repeatCount(): number {return this._repeatCount;}
+    public get speed(): number {return this._speed;}
+    public get distanceThreshold(): number {return this._distanceThreshold;}
+    public get repeat(): boolean {return (this._repeatCount > 0 || this._repeatCount == -1)}
+
+    public used() {if(this._repeatCount > 0){this._repeatCount--;}}
+}
