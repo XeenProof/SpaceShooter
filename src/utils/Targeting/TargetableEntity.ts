55752b6400fefe8cfe6646da66250ac7aa9bb77f
XeenProof <whosejimmy@gmail.com>
2023-03-28 09:28:43 -0400

    Basic Targing functions init

---
+++ src/utils/Targeting/TargetableEntity.ts
@@ -0,0 +1,12 @@
+import Positioned from "../../Wolfie2D/DataTypes/Interfaces/Positioned";
+import { TargetingEntity } from "./TargetingEntity";
+
+export interface TargetableEntity extends Positioned {
+
+    getTargeting(): TargetingEntity[];
+
+    addTargeting(targeting: TargetingEntity): void;
+
+    removeTargeting(targeting: TargetingEntity): void;
+
+}
