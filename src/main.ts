f472ada432f3d9026640c51f280ff1887a14bab4
XeenProof <whosejimmy@gmail.com>
2023-04-23 17:40:42 -0400

    show debug false

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ import { Inputs, cheats } from "./constants/gameoptions";
         clearColor: {r: 0.1, g: 0.1, b: 0.1},   // The color the game clears to
         inputs: Inputs,
         useWebGL: false,                        // Tell the game We hate Webgl
-        showDebug: true                       // Whether to show debug messages. You can change this to true if you want
+        showDebug: false                       // Whether to show debug messages. You can change this to true if you want
     }
 
     // Create a game with the options specified
