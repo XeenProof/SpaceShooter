341aa180efad120eb432672bc5345ed0f81dc7c2
XeenProof <whosejimmy@gmail.com>
2023-05-08 10:40:58 -0400

    minor clean up

--- src/game/scenes/MenuScenes/MainMenu.ts
+++ src/game/scenes/MenuScenes/MainMenu.ts
@@ -62,7 +62,6 @@ export default class MainMenu extends Scene {
     private musicPlaying:boolean
 
     public override initScene(init: Record<string, any> = {}): void {
-        console.log("init called")
         this.musicPlaying = (init.musicPlaying)?init.musicPlaying:false
     }
 
